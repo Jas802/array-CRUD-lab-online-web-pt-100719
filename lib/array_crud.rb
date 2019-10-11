90ad94bbb81c2346306ef0b239ba50d41213260e
Jas Spencer <jas.spencer414@gmail.com>
2019-10-11 17:49:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+quarter_backs = ["Tom Brady", "Aaron Rodgers", "Russel Wilson", "Drew Brees"]
 end
 
 def add_element_to_end_of_array(array, element)
